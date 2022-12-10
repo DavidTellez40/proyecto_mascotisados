5733149fe02ff70ade2440fbd242ebdeb3358628
Edgar David Tellez <109888126+DavidTellez40@users.noreply.github.com>
2022-12-10 06:00:01 -0500

    Add files via upload

---
+++ PROYECTO_MASCOTISADOS/controllers/Landing.php
@@ -0,0 +1,12 @@
+<?php
+    
+    //echo "Estoy en el controlador Landing.php";
+    class Landing{
+        function _construct(){}
+        function index(){
+            require_once "views/business/index.view.php";
+            //echo "Estoy en el controlador Landing.php";
+        }
+    }
+
+?>
---
+++ PROYECTO_MASCOTISADOS/views/business/index.view.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="assets/css/styles.css">
+</head>
+
+<body>
+    <!--contenedor-->
+    <div class="contenedor">
+        <!--Navegador-->
+        <nav>
+            <div class=" logo"></div>
+            <div class=" menu"></div>
+            <div class=" ingreso"></div>
+        </nav>
+        <!--Encabezado-->
+        <header class="borde"></header>
+        <!--Portafolio-->
+        <div class="borde titulo"></div>
+        <section>
+            <div class="borde portafolio1"></div>
+            <div class="borde portafolio1"></div>
+            <div class="borde portafolio1"></div>
+        </section>
+        <!--Nosotros-->
+        <div class="borde titulo"></div>
+        <section>
+            <div class="nos-fila">
+                <div class="borde nos-col-ext"></div>
+                <div class="borde nos-col-cnt"></div>
+                <div class="borde nos-col-ext"></div>
+            </div>
+            <div class="nos-fila">
+                <div class="borde nos-col-ext"></div>
+                <div class="borde nos-col-cnt"></div>
+                <div class="borde nos-col-ext"></div>
+            </div>
+            <div class="nos-fila">
+                <div class="borde nos-col-ext"></div>
+                <div class="borde nos-col-cnt"></div>
+                <div class="borde nos-col-ext"></div>
+            </div>
+        </section>
+        <!--Contáctanos-->
+        <div class="borde titulo"></div>
+        <section>
+            <div class="borde con-fila"></div>
+            <div class="con-fila">
+                <div class="borde con-col-arr"></div>
+                <div class="borde con-col-aba"></div>
+            </div>
+        </section>
+        <!--Pie de página-->
+        <footer class="borde"></footer>
+    </div>
+    <script src="assets/js/scripts.js"></script>
+</body>
+
+</html>
